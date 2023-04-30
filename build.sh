beb036a19c6a0f033e338cbf472743112461f5db
ms <schaefer9111@gmail.com>
2023-04-30 19:47:47 +0200

    vault backup: 2023-04-30 19:47:47

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Single Pages
-#pandoc .pandoc-config.yml $1 -o output/$1.html --standalone -C --file-scope
+pandoc .pandoc-config.yml $1 -o output/$1.html --standalone -C --file-scope
+
+#all to html
+# pandoc .pandoc-config.yml Chapters/* -o output/all-cwc.html -C --file-scope --resource-path=./Images/original/
 
 # all to pdf
-pandoc .pandoc-config.yml Chapters/* -o output/all-cwc.pdf --pdf-engine=xelatex --standalone -C --file-scope --resource-path=./Images/
+#pandoc .pandoc-config.yml Chapters/* -o output/all-cwc.pdf --pdf-engine=xelatex --standalone -C --file-scope --resource-path=./Images/
 
 
 # pandoc .pandoc-config.yml Chapters/* -o output/all-cwc.html --pdf-engine=xelatex --standalone -C --file-scope --resource-path=./Images/
